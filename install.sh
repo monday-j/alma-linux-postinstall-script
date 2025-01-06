58b767e9ede174944b20d9e992f2c41cf6f8bccd
monday-j <github@mondayj.net>
2025-01-06 21:26:12 +1100

    changed docker repo from RHEL to CentOS

--- install.sh
+++ install.sh
@@ -32,7 +32,7 @@ sudo mv micro /usr/bin
 # set up docker
 echo "Installing Docker..."
 sudo dnf -y install dnf-plugins-core
-sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo usermod -aG docker $USER
 sudo systemctl enable --now docker
